daa8927a22a73596180be52c4ff409be1364cc3b
Megunimine-chan <inactionmovie@ukr.net>
2021-12-08 02:06:25 -0800

    Init

---
+++ Library.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void print_lab_info() {
+    std::cout << "Library\n";
+    std::cout << "END\n";
+}
---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Library.cpp"
+
+int main() {
+    std::cout << "It is my 2 lab\n";
+    std::cout << "Start\n";
+    print_lab_info();
+    return 0;
+}
